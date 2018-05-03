37b000d33cabbd7277fc8c764d9d34082da69d9e
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-03 13:15:11 +0200

    QuerySelector ID and Class - and STYLE

--- app.js
+++ app.js
@@ -14,7 +14,15 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 1;
 
-dice = Math.floor((Math.random() * 6) + 1);
-console.log(dice);
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+var x = document.querySelector('#score-1').textContent;
+console.log(x);
+
+
+document.querySelector('.dice').style.display = 'none';
