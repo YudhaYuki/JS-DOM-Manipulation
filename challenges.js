8a1b039ceb56ab8e6117787edcdaaed6b046d3f0
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-04 11:17:48 +0200

    Add another dice, so that there are two dices. The player looses his current score when one of them is a 1.

--- challenges.js
+++ challenges.js
@@ -28,15 +28,18 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
     if (gamePlaying) {
         // 1. Random number
-        var dice = Math.floor(Math.random() * 6) + 1;  
+        var dice1 = Math.floor(Math.random() * 6) + 1;  
+        var dice2 = Math.floor(Math.random() * 6) + 1;          
 
         // 2. Display the result
-        var diceDOM = document.querySelector('.dice');
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block';        
+        document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+        document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';        
 
         // 3. Update the round score IF the rolled number was NOT a 1
 
+        /*
         if (dice === 6 && lastDice === 6) {
             // player looses his score
             scores[activePlayer] = 0;
@@ -52,6 +55,16 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         }
 
         lastDice = dice;
+        */
+
+        if (dice1 !== 1 && dice2 !== 1) {
+            // Add score
+            roundScore += dice1 + dice2;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            // Next player
+            nextPlayer();
+        }
     }
 });
 
@@ -84,7 +97,8 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
             // Show WINNER pops up when reach cetrain amount of points
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             // Hiding the DICE
-            document.querySelector('.dice').style.display = 'none';
+            document.getElementById('dice-1').style.display = 'none';
+            document.getElementById('dice-2').style.display = 'none';   
             // Adding winner class and removing active player
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');   
@@ -118,7 +132,8 @@ function nextPlayer() {
     document.querySelector('.player-1-panel').classList.toggle('active');
     
     // Hidden the dice before another player plays
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none'; 
 }
 
 // Reset everything WHEN we click NEW GAME button
@@ -136,7 +151,8 @@ function init() {
 
     // var x = document.querySelector('#score-1').textContent;
 
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
